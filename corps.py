2584fe8982c239397c4f62dcdbf4afb745dc08d4
Kouton Vignon <esmelyann@gmail.com>
2025-12-21 18:38:04 +0000

    Add files via upload

--- corps.py
+++ corps.py
@@ -70,3 +70,6 @@ async def feedback(request: Request):
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
+    if __name__ == "__main__":
+        port = int(os.environ.get("PORT", 8000))
+        uvicorn.run(app, host="0.0.0.0", port=port)
